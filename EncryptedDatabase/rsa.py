2fa66c5402b87ad44d9e8eb13d65b9ad031bcbc1
BiancaPricopi <pb31627@gmail.com>
2022-12-09 19:45:33 +0200

    [ED]: Added docstrings for RSA

--- EncryptedDatabase/rsa.py
+++ EncryptedDatabase/rsa.py
@@ -6,17 +6,36 @@ from Crypto.Util import number
 
 
 def get_prime_of_length(length):
+    """
+    Get a prime number of a specific length.
+
+    :param length: length of the number
+    :return: a prime number of a specific length
+    """
     return number.getPrime(length)
 
 
 def gcd(a, b):
+    """
+    Compute greatest common divisor.
+
+    :param a: first number
+    :param b: second number
+    """
     while b != 0:
         a, b = b, a % b
     return a
 
 
 def gcd_extended(a, b):
-    # return (g, x, y) a.i. a*x + b*y = g = gcd(a, b)
+    """
+    Extended Euclidean algorithm.
+
+    :param a: first number
+    :param b: second number
+    :return: a tuple containing gcd and Bezout coefficients
+    """
+    # (g, x, y) a.i. a*x + b*y = g = gcd(a, b)
     old_s, s = 1, 0
     old_t, t = 0, 1
     while b != 0:
@@ -27,15 +46,34 @@ def gcd_extended(a, b):
 
 
 def multiplicative_inverses(a, b):
+    """
+    Computes the multiplicative inverse.
+
+    :param a: first number
+    :param b: second number
+    :return: the multiplicative inverse
+    """
     _, x, _ = gcd_extended(a, b)
     return x % b
 
 
 def generate_e(phi):
+    """
+    Generates a random number smaller than phi.
+
+    :param phi: phi number
+    :return: a random number smaller than phi
+    """
     return rand.randrange(2, phi - 1)
 
 
 def generate_keys(bits):
+    """
+    Generates public key and private key for RSA.
+
+    :param bits: the length for p and q
+    :return: two tuples representing public key and private key
+    """
     p = get_prime_of_length(bits)
     q = get_prime_of_length(bits)
     while p == q:
@@ -58,21 +96,41 @@ def generate_keys(bits):
     return (n, e), (n, d)
 
 
-# c = m^e mod n
 def encrypt(public_key, plain_text):
+    """
+    RSA encryption.
+
+    :param public_key: the public key
+    :param plain_text: message to be encrypted
+    :return: ciphertext
+    """
     n, e = public_key
+    # c = m^e mod n
     ciphertext = [pow(ord(m), e, n) for m in plain_text]
     return ciphertext
 
 
-# m = c^d mod n
 def decrypt(private_key, ciphertext):
+    """
+    RSA decryption.
+
+    :param private_key: the private key
+    :param ciphertext: ciphertext to be decrypted
+    :return: plaintext
+    """
     n, d = private_key
+    # m = c^d mod n
     plain_text = [chr(pow(c, d, n)) for c in ciphertext]
     return ''.join(plain_text)
 
 
 def read_file(file):
+    """
+    Reads content of a file.
+
+    :param file: the absolute path of the file
+    :return: the content of the file
+    """
     try:
         with open(file, 'r') as file:
             content = file.read()
@@ -82,6 +140,12 @@ def read_file(file):
 
 
 def write_file(content, filename):
+    """
+    Writes content into a file.
+
+    :param content: the content of the file
+    :param filename: the absolute path of the file
+    """
     try:
         path = os.path.join(sys.argv[2], filename)
         with open(path, 'w') as file:
